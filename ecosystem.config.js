12580beaba1b5c06566c787fba3782994eaf1650
saravana <saravanakumar.ravichandran@codingmart.com>
2020-03-16 00:02:09 +0530

    user management

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,8 +29,8 @@ module.exports = {
                         && npx sequelize-cli db:migrate:undo --name 20200125113725-create-order-items 
                         && npx sequelize-cli db:migrate:undo --name 20200119124739-create-user
                         && npx sequelize-cli db:migrate --env production 
-                        && pm2 reload ecosystem.config.js --env production
-                        && npm start`
+                        && npm start
+                        && pm2 reload ecosystem.config.js --env production`
     }
   }
 };
